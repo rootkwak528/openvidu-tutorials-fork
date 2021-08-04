8e5c78e64acba1acf24a553621d626fc1cdac839
Root_Kwak <rootkwak528@gmail.com>
2021-08-04 14:16:17 +0900

    add debug log

--- openvidu-insecure-vue/src/components/OvVideo.vue
+++ openvidu-insecure-vue/src/components/OvVideo.vue
@@ -11,7 +11,11 @@ export default {
 	},
 
 	mounted () {
-		this.streamManager.addVideoElement(this.$el);
+    // this.streamManager.addVideoElement(this.$el);
+
+    // 스트림 안 되는 문제 파악용 debug
+		const addVideoElementStatus = this.streamManager.addVideoElement(this.$el);
+    console.log(addVideoElementStatus)
 	},
 };
 </script>
