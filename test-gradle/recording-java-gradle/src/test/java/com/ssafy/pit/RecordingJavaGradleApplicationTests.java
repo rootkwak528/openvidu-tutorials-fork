b3bc7ad9f32f62ee0a48f02bc15db61da2205393
서민영 <tjalsdud9@gmail.com>
2021-08-05 22:29:00 +0900

    Delete openvidu-recording-java & Create recording-java-gradle

---
+++ test-gradle/recording-java-gradle/src/test/java/com/ssafy/pit/RecordingJavaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.pit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingJavaGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
