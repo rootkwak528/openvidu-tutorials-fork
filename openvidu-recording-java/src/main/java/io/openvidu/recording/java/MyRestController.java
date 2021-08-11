51d428ebb6dbc0a69ef5cebbc50c2c4fc73ddad3
서민영 <tjalsdud9@gmail.com>
2021-08-11 16:27:32 +0900

    Create Room Comtroller

--- openvidu-recording-java/src/main/java/io/openvidu/recording/java/MyRestController.java
+++ openvidu-recording-java/src/main/java/io/openvidu/recording/java/MyRestController.java
@@ -7,10 +7,12 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // json
@@ -47,7 +49,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 
-
 @RestController
 @RequestMapping("/api")
 public class MyRestController {
@@ -68,12 +69,42 @@ public class MyRestController {
 	// Secret shared with our OpenVidu server
 	private String SECRET;
 
+	// json 사용을 위한 gson 선언
+	// Gson gson = new Gson();
+	
+	// room 생성 시 정보
+//	String sName, nName;
+	
 	public MyRestController(@Value("${openvidu.secret}") String secret, @Value("${openvidu.url}") String openviduUrl) {
 		this.SECRET = secret;
 		this.OPENVIDU_URL = openviduUrl;
 		this.openVidu = new OpenVidu(OPENVIDU_URL, SECRET);
 	}
 
+	/*******************/
+	/*** Create Room ***/
+	/*******************/
+	// https://i5a204.p.ssafy.io:5000?sessionId=blahblahblabh&name=hogeun/
+//	@GetMapping
+//	// public ResponseEntity<JsonObject> getRoom(@RequestParam Map<String, String> sessionName, @RequestParam Map<String, String> nickName) {
+//	public void getRoom(@RequestParam Map<String, String> sessionName, @RequestParam Map<String, String> nickName) {
+//				
+//		sName = (String) sessionName.get("sessionName");
+//		nName = (String) nickName.get("connectionId");
+//		
+//		System.out.println("sessionName: " + sName + ", nickName: " + nName);
+//		
+//		// Gson gson = new Gson();
+////		JsonObject json = new JsonObject();
+////		json.addProperty("sessionName", sName);
+////		json.addProperty("nickName", nName);
+//		
+//		// return new ResponseEntity<>(json, HttpStatus.OK);
+//	}
+	
+	
+	
+	
 	/*******************/
 	/*** Session API ***/
 	/*******************/
