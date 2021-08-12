aef63215de121e87263f121caf036d6bebc1678c
Root_Kwak <rootkwak528@gmail.com>
2021-08-13 01:46:00 +0900

    video grid

--- openvidu-recording-java/src/main/resources/static/app.js
+++ openvidu-recording-java/src/main/resources/static/app.js
@@ -570,8 +570,8 @@ function ondblclickVideo(target) {
 			// 하이라이트 비디오 일반 비디오로 옮기기
 		// 타겟 비디오 하이라이트 비디오로 옮기기
 	
-	if (!dblclickNodes.has(target)) {
-		dblclickNodes.add(target)
+	if (!dblclickNodes.has(target.id)) {
+		dblclickNodes.add(target.id)
 		target.addEventListener('dblclick', function(event) {
 			ondblclickVideo(event.target)
 		})
