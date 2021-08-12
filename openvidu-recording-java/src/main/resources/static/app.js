cffd3ac85deea1ce9cda889ff96bd8528375118d
Root_Kwak <rootkwak528@gmail.com>
2021-08-12 20:49:19 +0900

    video grid

--- openvidu-recording-java/src/main/resources/static/app.js
+++ openvidu-recording-java/src/main/resources/static/app.js
@@ -515,16 +515,16 @@ function updateNumVideos(i) {
 	const winHeight = window.innerHeight
 	console.log(winWidth, winHeight, numVideos)
 
-	// let colNumPlusOne = colNum + 1
-	// let rowNum = Math.ceil(numVideos / colNumPlusOne)
-	// let videoSizeX = (winWidth - 10 * (colNumPlusOne - 1)) / colNumPlusOne
+	let colNumPlusOne = colNum + 1
+	let rowNum = Math.ceil(numVideos / colNumPlusOne)
+	let videoSizeX = (winWidth - 10 * (colNumPlusOne - 1)) / colNumPlusOne
 
-	// while (rowNum * videoSizeX + 10 * (rowNum - 1) <= winHeight) {
-	// 	colNumPlusOne = colNum + 1
-	// 	rowNum = Math.ceil(numVideos / colNumPlusOne)
-	// 	videoSizeX = (winWidth - 10 * (colNumPlusOne - 1)) / colNumPlusOne
-	// 	colNum = colNumPlusOne
-	// }
+	while (rowNum * videoSizeX + 10 * (rowNum - 1) <= winHeight) {
+		colNum = colNumPlusOne
+		colNumPlusOne = colNum + 1
+		rowNum = Math.ceil(numVideos / colNumPlusOne)
+		videoSizeX = (winWidth - 10 * (colNumPlusOne - 1)) / colNumPlusOne
+	}
 
 	$('#video-container').css('grid-template-columns', `repeat(${colNum}, 1fr)`)
 }
